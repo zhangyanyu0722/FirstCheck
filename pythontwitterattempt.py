aa47bb744bfdbd22fc1b90aa20bc0f643bca8f04
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-09-18 00:50:05 -0400

    pythontwitterattempt
    Test file used to see how branches work in GitHub. Can be used as actual code file for project later on.

---
+++ pythontwitterattempt.py
@@ -0,0 +1,6 @@
+import twitter
+
+api = twitter.Api(consumer_key='do not write a key here',
+  consumer_secret='do not write secret here',
+    access_token_key='do not write actual key here',
+    access_token_secret='do not write actial secret here')

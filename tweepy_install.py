b1470dc216e696c2e1c69b870e41ad6b8276898a
YANYU ZHANG <55149684+zhangyanyu0722@users.noreply.github.com>
2019-09-24 17:15:17 -0400

    Create tweepy_install.py

---
+++ tweepy_install.py
@@ -0,0 +1,13 @@
+# The easiest way to install the latest version is by using pip/easy_install to pull it from PyPI:
+  pip install tweepy
+  
+# You may also use Git to clone the repository from GitHub and install it manually:
+  git clone https://github.com/tweepy/tweepy.git
+  cd tweepy
+  python setup.py install
+  
+  
+  
+# A good website for using tweepy:  https://tweepy.readthedocs.io/en/latest/
+
+

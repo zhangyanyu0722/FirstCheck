db42b108e46329ba1841cadf859be637b987c9a8
Jennifer Campbell <48844772+mysteriousmartel@users.noreply.github.com>
2019-09-29 20:23:38 -0400

    Add files via upload
    Will be using IBM API instead of Google's API. Still needs to be adapted to take in texts as arguments.

---
+++ ibmkeysample.py
@@ -0,0 +1,15 @@
+import json
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_watson.natural_language_understanding_v1 import Features, KeywordsOptions
+
+natural_language_understanding = NaturalLanguageUnderstandingV1(
+    version='2019-07-12',
+    iam_apikey='Qn0cd8S-jTG4fDKwStufeKgIsylXI2EfIjgWYoAeT2o9',
+    url='https://gateway-wdc.watsonplatform.net/natural-language-understanding/api'
+)
+
+response = natural_language_understanding.analyze(
+    url='www.ibm.com',
+    features=Features(keywords=KeywordsOptions(sentiment=True,emotion=True,limit=20))).get_result()
+
+print(json.dumps(response, indent=2))
